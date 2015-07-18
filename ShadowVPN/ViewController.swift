44ed463908577ed31e90d9aa35655812bf9e46d5
clowwindy <clowwindy42@gmail.com>
2015-07-18 15:49:45 +0800

    Initial Commit

---
+++ ShadowVPN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowVPN
+//
+//  Created by clowwindy on 7/18/15.
+//  Copyright © 2015 clowwindy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
